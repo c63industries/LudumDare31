ce003c9ae6dc8a2eea96c93a3e55fc4fe5c4c888
Charlaxy <charlaxy@gmail.com>
2014-12-06 13:06:39 -0600

    Started putting dialogue in format like IntroDuckTion.cs instead of the Script.md

---
+++ C63.LudumDare31.Game/Characters/Callers/IntroDuckTion.cs
@@ -0,0 +1,70 @@
+﻿using C63.LudumDare31.Game.Data;
+
+namespace C63.LudumDare31.Game.Characters.Callers
+{
+    public class Example : Base
+    {
+        public Example()
+            : base()
+        {
+
+            this.Dialog.Add
+            (
+                "North Pole, how may I help you?",
+                "Quack!",
+                () =>
+                {
+                    this.Dialog.Add
+                        (
+                            "What?",
+                            "QUACK!"
+                        );
+
+                    Answer answer = new Answer(() =>
+                            {
+                                this.Dialog.Add
+                                (
+                                    "Would you like to speak to Mr. Claws?",
+                                    "Quack.",
+                                    () =>
+                                    {
+                                        this.Dialog.Add
+                                            (
+                                                "I'm sorry, I can't put a duck through to my boss.",
+                                                "QUUUUUUUUUAAAACK",
+                                                () =>
+                                                {
+                                                    this.Dialog.Add
+                                                        (
+                                                            "I have to hang up now.",
+                                                            "Quackquackquack!"
+                                                        );
+                                                }
+                                            );
+                                        this.Dialog.Add
+                                            (
+                                                "Okay, I'll put you through.",
+                                                "Quackquack."
+                                            );
+                                    }
+                                );
+                                return "QUACKQUACKQUACKQUACKQUACK";
+                            }
+                    );
+
+                    this.Dialog.Add
+                        (
+                            "Is this a duck?",
+                            answer 
+                        );
+                    this.Dialog.Add
+                        (
+                            "What's wrong with your voice?",
+                            answer
+                        );
+                }
+
+            );
+        }
+    }
+}
---
+++ C63.LudumDare31.Game/Characters/Callers/Welcome.cs
@@ -0,0 +1,18 @@
+﻿using C63.LudumDare31.Game.Data;
+
+namespace C63.LudumDare31.Game.Characters.Callers
+{
+    public class Example : Base
+    {
+        public Example()
+            : base()
+        {
+
+            this.Dialog.Add
+            (
+                "Hello?",
+                "Mae govannen! This is Ciron, first counsel of the North Pole. As a new Ambassador and Liason for Lord Claws, it is your job to keep him pleased by diligently screening all of his callers. You must use your own good judgment as a Creature of the North to determine who is worthy to speak to the Bearded One in this trying time of Winter Solstice. Quel marth!"
+            );
+        }
+    }
+}
