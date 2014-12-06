215b2bb65896df5a9b9d844ff2c7c58a44ca8d4b
Wolfenhex <wolfenhex@gmail.com>
2014-12-06 08:26:25 -0600

    Created a project and solution to get started!

---
+++ C63.LudumDare31.Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace C63.LudumDare31.Game
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Application.Run(new Phone());
+        }
+    }
+}
