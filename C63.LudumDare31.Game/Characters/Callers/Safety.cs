3ea7ffd950c56144319f78e917fc2513e57760a0
Charlaxy <charlaxy@gmail.com>
2014-12-08 17:10:13 -0600

    Added HR.cs and Safety.cs

---
+++ C63.LudumDare31.Game/Characters/Callers/Safety.cs
@@ -0,0 +1,18 @@
+﻿using C63.LudumDare31.Game.Data;
+namespace C63.LudumDare31.Game.Characters.Callers
+{
+    public class Safety : Base
+    {
+        public static Safety safetyHappiness;
+
+        public Safety()
+            : base()
+        {
+            safetyHappiness = this;
+
+            this.Dialog.Add
+                (
+                );
+        }
+    }
+}
